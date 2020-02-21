6cd820483f4f88f2594d268ed906f578d711bd58
KalimohTraining <48172826+KalimohTraining@users.noreply.github.com>
2020-02-22 01:46:54 +0530

    Updated the path for the file to correct a typo

--- src/test/java/examples/performance/UserSimulation.scala
+++ src/test/java/examples/performance/UserSimulation.scala
@@ -7,7 +7,7 @@ import scala.concurrent.duration._
 
 class UserSimulation extends Simulation{
 
-  val getSingleUser = scenario("GET method should return 200").exec(karateFeature("classpath:examples.performance/GETSingleUser.feature"))
+  val getSingleUser = scenario("GET method should return 200").exec(karateFeature("classpath:examples/performance/GETSingleUser.feature"))
 
   setUp(
     getSingleUser.inject(rampUsers(10).during(10 seconds))
